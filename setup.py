321c637713d68d21f5c355bdf1d5ef7db76e6c0c
ziawang <850933437@qq.com>
2023-11-28 17:01:22 +0800

    新增setup.py和CHANGELOG.md

---
+++ setup.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+import os
+
+from setuptools import setup, find_packages
+
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def get_package_info(item, default=None):
+    scope = {}
+
+    data = None
+    file = os.path.join(THIS_DIR, "jqtrade", "__init__.py")
+    if os.path.exists(file):
+        with open(file) as fp:
+            exec(fp.read(), scope)
+        data = scope.get(item)
+
+    return data or default
+
+
+def get_long_description():
+    with open(os.path.join(THIS_DIR, "README.md"), "rb") as f:
+        long_description = f.read().decode("utf-8")
+    return long_description
+
+
+def _parse_requirement_file(path):
+    if not os.path.isfile(path):
+        return []
+    with open(path) as f:
+        requirements = [line.strip() for line in f if line.strip()]
+    return requirements
+
+
+def get_install_requires():
+    requirement_file = os.path.join(THIS_DIR, "requirements.txt")
+    return _parse_requirement_file(requirement_file)
+
+
+author = get_package_info("__author__"),
+email = get_package_info("__email__")
+
+
+setup(
+    name="jqtrade",
+    version=get_version(),
+    description="jqtrade<Simple trading framework on the user's local computer>",
+    packages=find_packages(exclude=("tests", "tests.*")),
+    author=author,
+    author_email=email,
+    maintainer=author,
+    maintainer_email=email,
+    license="Apache License v2",
+    package_data={'': ['*.*']},
+    url="https://www.joinquant.com",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    install_requires=get_install_requires(),
+    zip_safe=False,
+    platforms=["all"],
+    python_requires=">=3.6.13",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: Unix",
+    ],
+    entry_points={
+        "console_scripts": [
+            "jqtrade = jqtrade.__main__:main",
+        ],
+    },
+)
+
+
+if __name__ == '__main__':
+    print(find_packages(exclude=("tests", "tests.*")))
